e7b0a298921bd8c09445fd9d3f4fdc83ce3a947a
Rafael Sergio <rafa.sergio.02@live.com>
2020-04-13 19:45:14 -0300

    prettier integration with eslint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = Router();
 
-routes.get("/", (req, res) => res.json({ message: "oii" }));
+routes.get('/', (req, res) => res.json({ message: 'oii' }));
 
 export default routes;
