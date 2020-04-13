6dbffe78f525cf18c9719db51f9ca69d1ba8e1b7
Rafael Sergio <rafa.sergio.02@live.com>
2020-04-13 19:11:50 -0300

    config eslint

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "oii" });
-});
+app.get("/", (req, res) => res.json({ message: "oii" }));
 
 app.listen(3333, () => {
   console.log("Server started on port 3333");
