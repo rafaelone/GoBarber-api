6bf5cec9943709fe2e49443f1c33157455339396
Rafael Sergio <rafa.sergio.02@live.com>
2020-04-18 13:17:32 -0300

    upadated migrations and Users model

--- src/database/migrations/1587220613793-CreateAppointments.ts
+++ src/database/migrations/1587220613793-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1587220613793
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
