45fe4ea6b961b8ea8d87bb638b21563167cbb545
Rafael Sergio <rafa.sergio.02@live.com>
2020-05-10 16:09:44 -0300

    saving token on database and created migration token

---
+++ src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+
+import ResetPasswordService from '@modules/users/services/ResetPasswordService';
+import { container } from 'tsyringe';
+
+export default class ResetPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { password, token } = req.body;
+
+    const resetPassword = container.resolve(ResetPasswordService);
+
+    await resetPassword.execute({ token, password });
+
+    return res.status(204).json();
+  }
+}
