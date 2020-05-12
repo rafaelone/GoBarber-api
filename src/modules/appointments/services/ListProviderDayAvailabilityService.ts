08c88f24cd77c2d1b9a0525fdef566cd62878d8a
Rafael Sergio <rafa.sergio.02@live.com>
2020-05-12 12:25:28 -0300

    listing available days

---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,62 @@
+import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate, isThisSecond, getHours } from 'date-fns';
+
+import IAppointmentsRepository from '../repositories/IAppointmentRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+  day: number;
+}
+
+type IResponse = Array<{
+  hour: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderDayAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        month,
+        year,
+        day,
+      },
+    );
+
+    const hoursStart = 8;
+
+    const eachHourArray = Array.from(
+      { length: 10 },
+      (_, index) => index + hoursStart,
+    );
+
+    const availability = eachHourArray.map(hour => {
+      const hasAppointmentInHour = appointments.find(
+        appointment => getHours(appointment.date) === hour,
+      );
+
+      return {
+        hour,
+        available: !hasAppointmentInHour,
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProviderDayAvailabilityService;
