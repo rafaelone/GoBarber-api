11e7a68891fb0c7144ae5112e5b5923042aa8c8d
Rafael Sergio <rafa.sergio.02@live.com>
2020-05-14 11:26:19 -0300

    using class-transformer

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
 
@@ -10,9 +11,7 @@ export default class ProfileController {
 
     const user = await showProfile.execute({ user_id });
 
-    delete user.password;
-
-    return res.json(user);
+    return res.json(classToClass(user));
   }
 
   public async update(req: Request, res: Response): Promise<Response> {
@@ -31,6 +30,6 @@ export default class ProfileController {
 
     delete user.password;
 
-    return res.json(user);
+    return res.json(classToClass(user));
   }
 }
--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -10,6 +11,6 @@ export default class UserAvatarController {
       user_id: req.user.id,
       avatarFilename: req.file.filename,
     });
-    return res.json(user);
+    return res.json(classToClass(user));
   }
 }
