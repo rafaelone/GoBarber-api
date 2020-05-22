225d24aa71e861c46acb88fc7327e2b07b134651
Rafael Sergio <rafa.sergio.02@live.com>
2020-05-22 15:27:12 -0300

    some adjusts

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import ListProviderAppointments from '@modules/appointments/services/ListProviderAppointmentsService';
+import { classToClass } from 'class-transformer';
 
 export default class ProviderAppointmentsController {
   public async index(req: Request, res: Response): Promise<Response> {
-    const { day, month, year } = req.body;
+    const { day, month, year } = req.query;
     const provider_id = req.user.id;
 
     const listProviderAppointments = container.resolve(
@@ -13,11 +14,11 @@ export default class ProviderAppointmentsController {
 
     const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
-    return res.json(appointments);
+    return res.json(classToClass(appointments));
   }
 }
